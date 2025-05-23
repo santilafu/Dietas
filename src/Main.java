f881083904ca9f3a7dcc20af7e9abd2009f6d09b
santilafu <santi10dy@gmail.com>
2025-05-23 12:23:31 +0200

    Creacion Clase Maincon metodo para conectar con MySQL

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
+//Creacion Clase Main
 public class Main {
+    public static void main(String[] args) {
+        //Llamamos al metodo para conectar la base de datos
+        ConexionBD.conectar();
+    }
 }
